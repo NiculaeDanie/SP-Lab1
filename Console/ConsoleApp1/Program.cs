68b31a82a524d9c36ef784dc43c402124396d6e3
DanielNicu <daniel.niculae01@e-uvt.ro>
2022-12-07 09:01:38 +0200

    Singleton

--- Console/ConsoleApp1/Program.cs
+++ Console/ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Domain;
 
+Book book = new Book("title");
+DocumentManager.getInstance().setBook(book);
 Section cap1 = new Section("Capitolul 1");
+book.add(cap1);
 Paragraph p1 = new Paragraph("Paragraph 1");
 cap1.add(p1);
 Paragraph p2 = new Paragraph("Paragraph 2");
@@ -9,14 +12,11 @@ Paragraph p3 = new Paragraph("Paragraph 3");
 cap1.add(p3);
 Paragraph p4 = new Paragraph("Paragraph 4");
 cap1.add(p4);
-Console.WriteLine("Printing without Alignment");
-Console.WriteLine();
-cap1.print();
 p1.setAlignStrategy(new AlignCenter());
 p2.setAlignStrategy(new AlignRight());
 p3.setAlignStrategy(new AlignLeft());
 Console.WriteLine();
 Console.WriteLine("Printing with Alignment");
 Console.WriteLine();
-cap1.print();
+DocumentManager.getInstance().getBook().print();
 
---
+++ Console/Domain/DocumentManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DocumentManager
+    {
+        private static DocumentManager instance;
+        private Book book;
+        private DocumentManager()
+        {
+
+        }
+        public static DocumentManager getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DocumentManager();
+            }
+            return instance;
+        }
+        public void setBook(Book book)
+        {
+            this.book = book; 
+        }
+        public Book getBook()
+        {
+            return this.book;
+        }
+    }
+}
